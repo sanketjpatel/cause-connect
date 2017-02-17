f8204535068b8f22ab3ff1c0cc32f8f9a2776cf7
Sanket Patel <c.sanket.patel@uptake.com>
2017-02-16 18:10:39 -0600

    feat(contribute): add text for mission

--- src/features/tabs/tabs.controller.js
+++ src/features/tabs/tabs.controller.js
@@ -8,8 +8,6 @@
         function TabsController($window, AuthService, $state, $ionicPush) {
             var tabsVm = this;
 
-            tabsVm.isSeeker = false;
-
             //tabsVm.validateLogin = validateLogin;
             tabsVm.registerForPush = registerForPush;
 
